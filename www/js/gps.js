1f7dc62f4f1d563b876dbb358fb17b9fd8b108ce
torlopov <i.torlopov@mail.ru>
2015-05-28 16:32:10 +0300

    10

---
+++ www/js/gps.js
@@ -0,0 +1,11 @@
+
+var exec = require('cordova/exec');
+
+var gpsDetect = function() {};
+
+gpsDetect.prototype.checkGPS = function(alertShown, successCallback, failureCallback) {
+	exec(successCallback, failureCallback, 'GpsDetector', 'gpsDetection', [alertShown]);
+};
+
+var gpsDetect = new gpsDetect();
+module.exports = gpsDetect;
